e9a04a368afbb3c85195dd788f11b2e0f85cfaf8
CaO2323 <132775838+CaO2323@users.noreply.github.com>
2023-07-27 01:28:09 +0200

    Add files via upload

---
+++ taskModel.php
@@ -0,0 +1,105 @@
+<?php
+
+
+class TaskModel {
+
+    private array $task_list;
+    protected $dataFilePath = ROOT_PATH.'/app/models/data/data.json';
+
+    public function newTask(string $title, string $textArea, string $user, string $status, mixed $datetime, mixed $endTime) {
+        
+        $newId = count($this->readData()) + 1;
+    
+        
+        if ($datetime === null) {
+            $datetime = date("M d, Y g:i A");
+        }
+        if ($endTime === null) {
+            $endTime = date("M d, Y g:i A");
+        }
+
+        
+        $currentDate = new DateTime();
+        $selectedDate = new DateTime($datetime);
+
+        if ($selectedDate < $currentDate) {
+            
+            throw new Exception("Error: The selected date cannot be earlier than today.");
+        }
+
+        $data = [
+            'id' => $newId,
+            'title' => $title,
+            'textarea' => $textArea,
+            'user' => $user,
+            'status'=> $status,
+            'datetime' => $datetime,
+            'endtime' => $endTime
+        ];
+
+        $tasks = $this->readData();
+        
+        $tasks[] = $data;
+
+        $this->saveData($tasks);
+    }
+
+    private function saveData($task_data) : void{
+            
+        file_put_contents($this->dataFilePath,json_encode($task_data, JSON_PRETTY_PRINT));
+    
+        }
+    
+    private function readData() {
+        // va al archivo json
+        $tasksData = file_get_contents($this->dataFilePath);
+        
+        // lee el archivo json y lo transforma 
+         return $data_read = json_decode($tasksData, true);
+    }
+    function getTaskById($id){
+    
+        $users= $this->readData();
+            //Buscar if en el array 
+        foreach($users as $user){
+            if($user['id'] == $id){
+                return $user;
+              
+            }
+        }
+        return null;
+    }
+       
+        public function getAllTasks()
+        {
+            return $this->readData();
+        }
+        
+        public function deleteTask($id){
+           
+           $tasks = $this->readData();
+           foreach ($tasks as $key => $task) {
+            if ($task['id'] == $id) {
+                unset($tasks[$key]);
+                break;
+            }
+        }
+            $this->saveData($tasks);
+    
+           }
+    
+           public function modifyTask($id, $title, $textArea, $user, $status, $startTime, $endTime) {
+        
+            $currentDate = new DateTime();
+            $selectedDate = new DateTime($startTime);
+    
+            if ($selectedDate < $currentDate) {
+                
+                throw new Exception("Error: The selected date cannot be earlier than today.");
+            }
+           
+        }
+    
+    }
+    
+    ?>
